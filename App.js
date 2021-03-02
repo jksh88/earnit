7cdb97ac016849c0bc1beccfac76a3beac4d70d0
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-02 17:40:05 -0500

    initial setup and test with simple code

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const textList = [
+  "it's good",
+  'haha',
+  'what up?',
+  'take it easy',
+  'chlll',
+  'How are you',
+];
+
 export default function App() {
+  const [text, setText] = useState(
+    'Open up App.js to start working on your app!'
+  );
+  const randomText = () => {
+    const rand = Math.floor(Math.random() * textList.length);
+    setText(textList[rand]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="Change Text" onPress={randomText} />
       <StatusBar style="auto" />
     </View>
   );
