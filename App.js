411238b33664d95ac2aed5662dfaa382634a2f1c
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-11 23:52:10 -0500

    wip

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const textList = [
   "it's good",
